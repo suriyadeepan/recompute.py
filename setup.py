f0a94352cce9fa6ca58c8e06ab959609a7c76802
Suriyadeepan Ramamoorthy <suriyadeepan.r@gmail.com>
2019-02-26 17:54:18 +0530

    package setup files

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="recompute",
+    version="0.0.7",
+    author="Suriyadeepan Ramamoorthy",
+    author_email="suriyadeepan.r@gmail.com",
+    description="Remote Computation Framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/suriyadeepan/recompute.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+      'console_scripts' : [ 'recompute=recompute.recompute:main' ],
+      },
+)
