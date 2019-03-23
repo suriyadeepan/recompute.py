8df3d07807c7b81cabb0c0ca5fff71f93902858b
Suriyadeepan Ramamoorthy <suriyadeepan.r@gmail.com>
2019-03-23 08:18:27 +0530

    adds include/exclude

--- recompute/bundle.py
+++ recompute/bundle.py
@@ -11,6 +11,8 @@ logger = logging.getLogger(__name__)
 # RSYNC DB
 RSYNC_DB = '.recompute/rsync.db'
 REQS = 'requirements.txt'
+INCLUDE = '.recompute/include'
+EXCLUDE = '.recompute/exclude'
 
 
 class Bundle(object):
@@ -22,9 +24,10 @@ class Bundle(object):
     self.name = name if name else self.path.split('/')[-1]
     # local db
     self.db = RSYNC_DB
-    # create local conf directory if it doesn't exist
-    if not os.path.exists(LOCAL_CONFIG_DIR):
-      os.makedirs(LOCAL_CONFIG_DIR)
+    # setup local conf
+    self.init_local_conf()
+    # init include/exclude files
+    self.init_include_exclude()
     # update bundle dependencies
     self.update_dependencies()
 
@@ -32,6 +35,8 @@ class Bundle(object):
     """ Update dependencies """
     # get a list of files (local dependencies)
     self.files = list(self.get_local_deps())
+    # add INCLUDE; remote EXCLUDE
+    self.files = self.inclusion_exclusion()
     logger.debug(' '.join(self.files))
     # create a file containing list of dependencies
     self.populate_requirements()
@@ -39,6 +44,27 @@ class Bundle(object):
     self.requirements = self.get_requirements()
     # create a file containting list of local dependencies
     self.populate_local_deps()
+
+  def init_local_conf(self):
+    # create local conf directory if it doesn't exist
+    if not os.path.exists(LOCAL_CONFIG_DIR):
+      os.makedirs(LOCAL_CONFIG_DIR)
+
+  def init_include_exclude(self):
+    if not os.path.exists(INCLUDE):
+      open(INCLUDE, 'w').close()
+    if not os.path.exists(EXCLUDE):
+      open(EXCLUDE, 'w').close()
+
+  def inclusion_exclusion(self):
+    # read from .recompute/include
+    include = [ f for f in open(INCLUDE).readlines() if f.strip() ]
+    # read from .recompute/exclude
+    exclude = [ f for f in open(EXCLUDE).readlines() if f.strip() ]
+    # update self.files
+    self.files = [ f for f in self.files if f not in exclude ]
+    self.files.extend(include)
+    return list(set(self.files))
 
   def get_local_deps(self):
     """ Run a search for *.py files in current directory """
--- tests/bundle.py
+++ tests/bundle.py
@@ -11,6 +11,24 @@ def test_get_local_deps(bundle):
   assert len(list(bundle.get_local_deps())) > 0
 
 
+def test_init_include_exclude(bundle):
+  from recompute.bundle import INCLUDE, EXCLUDE
+  import os
+  assert os.path.exists(INCLUDE)
+  assert os.path.exists(EXCLUDE)
+
+
+def test_inclusion_exclusion(bundle):
+  from recompute.bundle import INCLUDE, EXCLUDE
+  with open(INCLUDE, 'a') as f:
+    f.write('README.md')
+  with open(EXCLUDE, 'a') as f:
+    f.write('recompute/utils.py')
+  bundle.update_dependencies()
+  assert 'README.md' in bundle.files
+  assert 'recompute/utils.py' not in bundle.files
+
+
 def test_get_requirements(bundle):
   assert len(bundle.get_requirements()) > 0
 
